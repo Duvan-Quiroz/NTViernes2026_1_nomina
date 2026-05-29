800c321f05a508da2d91b4dcd497b86c31fbe353
julian ciro <julianciro0202@gmail.com>
2026-05-29 17:50:45 -0500

    feat/se crean las rutinas faltantes de bonificacion

---
+++ notebook/transformacion_bonificacion.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def transformar_datos(data_frame_limpio):
+
+    # Transformación 1 (conteo de bonificaciones por id)
+    filtro1 = data_frame_limpio
+    agrupacion1 = filtro1.groupby("id")["valor"].count().reset_index(name="cantidad_bonificaciones")
+
+    # Transformación 2 (valor promedio por id)
+    filtro2 = data_frame_limpio
+    agrupacion2 = filtro2.groupby("id")["valor"].mean().reset_index(name="valor_promedio")
+
+    # Transformación 3 (bonificaciones con valor alto >300)
+    filtro3 = data_frame_limpio.query("valor > 300")
+    agrupacion3 = filtro3.groupby("id")["valor"].count().reset_index(name="cantidad_valor_alto")
+
+    # Transformación 4 (rangos de valor)
+    filtro4 = data_frame_limpio.copy()
+    filtro4["rango_valor"] = pd.cut(
+        filtro4["valor"],
+        bins=[0, 200, 350, 500],
+        labels=["Bajo", "Medio", "Alto"]
+    )
+    agrupacion4 = filtro4.groupby("rango_valor")["id"]\
+        .count().reset_index(name="cantidad")
+
+    # Transformación 5 (total bonificado por id)
+    filtro5 = data_frame_limpio
+    agrupaci
